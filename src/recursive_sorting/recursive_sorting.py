7f278111c0ab5760817d133d4691270b462017df
Derek Etman <dereketman@gmail.com>
2020-01-22 09:12:29 -0800

    + Merge doesnt add last answer still

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,38 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helpcd  function below to merge 2 sorted arrays
+arr1 = [1,3,5]
+arr2 = [2,4,6]
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+    a_index = 0
+    b_index = 0
+        
+    # Loop through the length of  the combined elements
+    for k in range(len(merged_arr) - 1):
+        if a_index > len(arrA):
+            merged_arr[k] = arrB[b_index]
+        elif b_index > len(arrB):
+            merged_arr[k] = arrA[a_index]
+        # if A[1] is smaller than B[1]
+        elif arrA[a_index] < arrB[b_index]:
+            # append A[1] to the merged array
+            merged_arr[k] = arrA[a_index]
+            a_index += 1
+        # else
+        else:
+            # append B[1] to the end of the merged array  
+            merged_arr[k] = arrB[b_index]
+            b_index += 1
+
+    print(merged_arr)
     return merged_arr
+
+merge(arr1,arr2)
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    # TO-DOz
 
     return arr
 
